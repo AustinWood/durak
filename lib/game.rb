96c320a455192b42d7ea5f6fd52be17e61c1a245
Austin Wood <austin@austinkwood.com>
2017-02-04 14:30:14 -0800

    Modify Game class for quick creation of new game with default players for testing purposes

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,23 @@
+require_relative 'player'
+
 class Game
   attr_reader :players
 
   def initialize(players)
     @players = players
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  players = [
+    Player.new("Vladimir Putin"),
+    Player.new("Pyotr Tchaykovsky"),
+    Player.new("Yuriy Gagarin"),
+    Player.new("Mikhail Gorbachov")
+  ]
+  game = Game.new(players)
+  puts "You initialized a new game with the following players:"
+  game.players.each do |player|
+    puts player.name
   end
 end
