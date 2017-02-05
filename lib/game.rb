9f5a8cfa34313a62135864a9055ce7b35710dc45
Austin Wood <austin@austinkwood.com>
2017-02-05 13:16:47 -0800

    Begin refactoring Game class

--- lib/game.rb
+++ lib/game.rb
@@ -2,52 +2,45 @@ require_relative 'player'
 require_relative 'deck'
 
 class Game
-  attr_reader :players, :trump_card
+  attr_reader :players
 
   def initialize(players)
     @deck = Deck.new
     @players = players
-    @trump_suit = nil
     @turns = 0
   end
 
   def play
     fill_hands
-    set_trump_card
+    set_trump_suit
     select_first_attacker
-    take_turn until loser? || @turns >= 100
-    declare_loser
+    take_turn until loser?
+    declare_durak
   end
 
   def fill_hands
     players.each { |player| player.fill_hand(@deck) }
   end
 
-  def set_trump_card
+  def set_trump_suit
     @trump_suit = @deck.reveal_trump_suit
     players.each { |player| player.remember_trump_suit(@trump_suit) }
     puts "The trump suit is #{@trump_suit}"
   end
 
   # Rotate the @players array so that the Player at i = 0
-  # is the player with the lowest card matching the trump suit
+  # is the Player with the lowest card matching the trump suit
   def select_first_attacker
     lowest_card_val = nil
     first_attacker_idx = 0
     players.each.with_index do |player, i|
-      player.cards.each do |card|
-        next unless card.suit == @trump_suit
-        if lowest_card_val.nil? || card.int_val < lowest_card_val
-          lowest_card_val = card.int_val
-          first_attacker_idx = i
-        end
+      player.trump_cards.each do |card|
+        next unless lowest_card_val.nil? || card.int_val < lowest_card_val
+        lowest_card_val = card.int_val
+        first_attacker_idx = i
       end
     end
     @players.rotate!(first_attacker_idx)
-  end
-
-  def loser?
-    @players.count <= 1
   end
 
   def take_turn
@@ -99,7 +92,11 @@ class Game
     players[1]
   end
 
-  def declare_loser
+  def loser?
+    @players.count <= 1
+  end
+
+  def declare_durak
     puts "----------------"
     if @players.count.zero?
       puts "There was no дурак this game!"
@@ -112,16 +109,7 @@ class Game
 end
 
 if __FILE__ == $PROGRAM_NAME
-  players = [
-    Player.new("Pyotr Ilyich Tchaikovsky"),
-    Player.new("Yuri Gagarin"),
-    Player.new("Vladimir Putin"),
-    Player.new("Mikhail Gorbachev")
-  ]
-  game = Game.new(players)
-  puts "You initialized a new game with the following players:"
-  game.players.each do |player|
-    puts player.name
-  end
-  game.play
+  names = ["Pyotr Ilyich Tchaikovsky", "Yuri Gagarin", "Vladimir Putin", "Mikhail Gorbachev"]
+  players = names.map { |name| Player.new(name) }
+  game = Game.new(players).play
 end
